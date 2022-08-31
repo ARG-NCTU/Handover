03ede8e1c4bf1ae717c78ab1632e759cb5c1032b
austin2408 <sdnd93612@gmail.com>
2022-08-31 14:03:13 +0800

    rename

--- record.sh
+++ record.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
-mkdir /home/kl/Pick-and-Place-with-RL/exp2/
+mkdir /home/dualarm/Pick-and-Place-with-RL/exp2/
 
-# rosbag record -o /home/kl/Pick-and-Place-with-RL/piyan/ --duration=1 \
-rosbag record -o /home/kl/Pick-and-Place-with-RL/exp2/ \
+rosbag record -o /home/dualarm/Pick-and-Place-with-RL/exp2/ \
     /camera_left/aligned_depth_to_color/image_raw/compressed \
     /camera_left/aligned_depth_to_color/camera_info \
     /camera_left/color/camera_info \
