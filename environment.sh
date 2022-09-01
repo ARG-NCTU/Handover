81a50b9a84a8b179626fc0d081ad9d5ad19b2263
jinglinjackychen <a2396660@gmail.com>
2022-09-01 10:25:32 +0800

    update

--- environment.sh
+++ environment.sh
@@ -4,26 +4,22 @@ if [ "$1" ]; then
     echo "ROS MASRER $1"
     export ROS_MASTER_URI=http://$1:11311
 else
-    # echo "ROS MASRER 127.0.0.1"
-    # export ROS_MASTER_URI=http://127.0.0.1:11311
-    echo "ROS MASRER 192.168.50.185"
-    export ROS_MASTER_URI=http://192.168.50.185:11311
+    echo "ROS MASRER 127.0.0.1"
+    export ROS_MASTER_URI=http://127.0.0.1:11311
 fi
 
 if [ "$2" ]; then
     echo "ROS IP $2"
     export ROS_IP=$2
 else
-    # echo "ROS IP 127.0.0.1"
-    # export ROS_IP=127.0.0.1
-    echo "ROS IP 192.168.50.161"
-    export ROS_IP=192.168.50.161
+    echo "ROS IP 127.0.0.1"
+    export ROS_IP=127.0.0.1
 fi
 
 source /opt/ros/melodic/setup.bash
 source ./catkin_ws/devel/setup.bash
 
 # source ~/.bashrc
-source ~/Handover/catkin_ws/devel/setup.bash
+source ~/handover-system/catkin_ws/devel/setup.bash
 
 echo "pick and place environment"
