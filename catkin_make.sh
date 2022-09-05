a6f1879ae7cacbf2d27aefcd21a64f76ef33f913
austin2408 <sdnd93612@gmail.com>
2022-09-05 15:04:29 +0800

    update

--- catkin_make.sh
+++ catkin_make.sh
@@ -1,26 +1,8 @@
 #! /bin/bash
 
-# CATKIN_IGNORE - mm-core 
-# touch ./catkin_ws/src/mm-core/arm_operation/abb_control/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/arm_operation/dual_arm_operation/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/arm_operation/ur5_arm_control/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/bin_picking/ddqn/grasp_suck/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/bin_picking/ddqn/visualization/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/bin_picking/ddqn/visual_system/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/pose_estimate/electronics_pick_and_assemble/pose_estimation/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/pose_estimate/electronics_pick_and_assemble/pose_estimate_and_pick/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/semantic_segmentation/electronics_pick_and_assemble/object_detection/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/semantic_segmentation/electronics_pick_and_assemble/object_detection_lily/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/system_fsm/barcode_flatten_system/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/system_fsm/pick_and_place_system/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/system_fsm/semantic_system/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/tools/CATKIN_IGNORE
-# touch ./catkin_ws/src/mm-core/semantic_segmentation/barcode_detection/CATKIN_IGNORE  
+source /opt/ros/melodic/setup.bash
+source ./catkin_ws/devel/setup.bash
 
-# CATKIN_IGNORE - algorithm 
-# touch ./catkin_ws/src/algorithm/CATKIN_IGNORE
-
-# python3 cv_bridge
 catkin_make --pkg vision_opencv -C ./catkin_ws \
     -DCMAKE_BUILD_TYPE=Release \
     -DPYTHON_EXECUTABLE=/usr/bin/python3 \
