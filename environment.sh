2b9aed5f4aa7f54ab1cd5e6ef2950e6e531d717f
austin2408 <sdnd93612@gmail.com>
2022-09-05 17:52:34 +0800

    update

--- environment.sh
+++ environment.sh
@@ -6,15 +6,26 @@ cwd=$PWD
 { read -r var2;} < <(yq -r .handover.workstation.ip_in $PWD/config/system-device.yaml)
 
 
-echo "ROS MASRER" $var1
-export ROS_MASTER_URI=http://$var1:11311
+
 
 if [ "$1" == 'nuc' ]; then
+    echo "ROS MASRER" $var1
+    export ROS_MASTER_URI=http://$var1:11311
+
     echo "ROS IP $var1"
     export ROS_IP=$var1
-else
+elif [ "$1" == 'ws' ]
+    echo "ROS MASRER" $var1
+    export ROS_MASTER_URI=http://$var1:11311
+
     echo "ROS IP $var2"
     export ROS_IP=$var2
+else
+    echo "ROS MASRER 127.0.0.1"
+    export ROS_MASTER_URI=http://127.0.0.1:11311
+
+    echo "ROS IP 127.0.0.1"
+    export ROS_IP=127.0.0.1
 fi
 
 source /opt/ros/melodic/setup.bash
